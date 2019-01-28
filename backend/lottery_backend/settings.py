b32e12df0d2b3b4ac973c380ac2a2489193f2229
Chinese-BruceWayne <1184249450@qq.com>
2019-01-29 03:00:42 +0800

    [update] config from sqlite==>mysql

--- backend/lottery_backend/settings.py
+++ backend/lottery_backend/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'lottery_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+from .secret import DB_SETTING
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': DB_SETTING["dbname"],
+        'USER': DB_SETTING["dbuser"],
+        'PASSWORD': DB_SETTING["dbpasswd"],
+        'HOST': "localhost",
+        'OPTIONS': {
+            "charset": "utf8"
+        }
     }
 }
 
