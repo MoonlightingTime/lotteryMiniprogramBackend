42ffbec16363d4b355cc1d095297d55325247b7d
Chinese-BruceWayne <1184249450@qq.com>
2019-02-01 01:13:39 +0800

    [fix] some backend bug

--- backend/init_database.sh
+++ backend/init_database.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-py="python"
-py3="python3"
-
-python=""
-if command -v $py >/dev/null 2>&1; then
-    python="$py"
-elif command -v $py3 > /dev/null 2>&1; then
-    python="$py3"
+if command -v python3 >/dev/null 2>&1; then
+    python="python3"
+elif command -v python > /dev/null 2>&1; then
+    python="python"
 else
     echo "python command is required for this script."
     exit 1
